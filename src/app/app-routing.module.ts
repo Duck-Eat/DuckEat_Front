6dbc74280456ea4e4ba446fa7d322044bf634e2b
Vincent <vincent.grande@outlook.fr>
2023-03-04 22:12:55 +0100

    added: auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,27 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import {AuthGuard} from "./guards/auth.guard";
 
 const routes: Routes = [
   {
+    path: 'authentication',
+    loadChildren: () => import('./pages/authentication/authentication.module').then( m => m.AuthenticationPageModule)
+  },
+  {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),
+    canActivate: [AuthGuard]
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
   },
+
   {
-    path: 'authentication',
-    loadChildren: () => import('./pages/authentication/authentication.module').then( m => m.AuthenticationPageModule)
-  },  {
     path: 'main',
     loadChildren: () => import('./pages/main/main.module').then( m => m.MainPageModule)
   },
-
-
 ];
 
 @NgModule({
